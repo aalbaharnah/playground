f6835abc1699b0e9cb2ff16fed24b8a64355bae1
aalbaharnah <baharnah.13@gmail.com>
2024-04-02 11:49:08 +0300

    navigation bar

--- src/app/game-button/index.tsx
+++ src/app/game-button/index.tsx
@@ -1,44 +1,76 @@
 import { Text, View, StyleSheet, Pressable, PressableProps } from "react-native";
 import Animated, {
-    Easing,
     interpolate,
-    interpolateColor,
     useAnimatedStyle,
     useSharedValue,
-    withSpring,
     withTiming,
 } from 'react-native-reanimated';
+import { Ionicons } from "@expo/vector-icons";
 const AnimatedPressable = Animated.createAnimatedComponent(Pressable);
 
 export default function GameButton() {
+
+    return (
+        <View className="flex-1">
+            <View className="flex-1">
+
+            </View>
+            <View className="flex-row items-center justify-center h-24 px-4 space-x-2 bg-slate-900">
+                <OptionButton style={styles.btn} title="الملاحظات" icon="clipboard-outline" />
+                <OptionButton style={styles.btn} title="الجدول" icon="today-outline" />
+                <OptionButton style={styles.btn} title="المهام" icon="golf-outline" />
+            </View>
+        </View>
+    )
+}
+
+interface OptionButtonProps extends PressableProps {
+    title: string;
+    onPress?: () => void;
+    borderColor?: string;
+    icon: string;
+}
+const OptionButton = ({ title, onPress, icon, style, ...rest }: OptionButtonProps) => {
     const press = useSharedValue(0);
 
     const animatedStyle = useAnimatedStyle(() => {
         return {
-            borderBottomWidth: interpolate(press.value, [0, 1], [7, 2]),
-            // borderColor: interpolateColor(press.value, [0, 1], ['#333', '#FFBB64']),
+            borderBottomWidth: interpolate(press.value, [0, 1], [5, 2]),
         };
     });
+
+    const onPressIn = () => {
+        press.value = withTiming(1, { duration: 200 })
+    }
+
+    const onPressOut = () => {
+        press.value = withTiming(0, { duration: 400 })
+    }
+
     return (
-        <View className="flex-1 items-center justify-center">
-            <AnimatedPressable
-                style={[animatedStyle, styles.btn]}
-                onPressIn={() => (press.value = withTiming(1, { duration: 200 }))}
-                onPressOut={() => (press.value = withTiming(0, { duration: 400 }))}
-            >
-                <Text className=" text-2xl">Press me</Text>
-            </AnimatedPressable>
-        </View>
+        <AnimatedPressable
+            style={[style, animatedStyle]}
+            onPressIn={onPressIn}
+            onPressOut={onPressOut}
+            onPress={onPress}
+            {...rest}
+        >
+            <Ionicons name={icon as 'today'} size={18} color="white" />
+            <Text className="text-white font-bold ml-1">{title}</Text>
+
+        </AnimatedPressable>
     )
 }
 
 const styles = StyleSheet.create({
     btn: {
-        borderTopWidth: 2,
-        borderLeftWidth: 2,
+        borderTopWidth: 1.4,
         borderRightWidth: 2,
-        paddingHorizontal: 32,
+        borderLeftWidth: 2,
+        paddingHorizontal: 16,
         paddingVertical: 8,
-        borderRadius: 16,
+        borderRadius: 12,
+        flexDirection: "row",
+        alignItems: "center",
     }
 })
